30eaf96519ae8c87c53af745020029084c870fba
glenn <glenn.ludszuweit@gmail.com>
2020-06-01 01:18:32 +0200

    monster rolodex final commit

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import { HeadTitle } from "./components/head-title/head-title.component";
 import "./App.css";
 
 class App extends Component {
@@ -18,6 +20,10 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -26,10 +32,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
+        <HeadTitle />
+        <SearchBox
           placeholder="search monster"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
---
+++ monster-rolodex/src/components/head-title/head-title.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./head-title.styles.css";
+
+export const HeadTitle = () => (
+  <h1 className="heading-title">Monster Rolodex</h1>
+);
