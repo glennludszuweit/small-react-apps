89659a15ea43665dd324fe9afa552ed2d09a0dab
glenn <glenn.ludszuweit@gmail.com>
2020-06-05 18:30:33 +0200

    adopt-me configured eslint to work with react

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,8 +1,10 @@
 import React from 'react';
 export default function Pet({ name, animal, breed }) {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, name),
-    React.createElement('h4', {}, animal),
-    React.createElement('h4', {}, breed),
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{animal}</p>
+      <p>{breed}</p>
+    </div>
+  );
 }
