281066b1bdabe165926539d7b78a3358d4718558
glenn <glenn.ludszuweit@gmail.com>
2020-06-07 00:19:50 +0200

    adopt-me created Carousel component to display animal photos and add to animal details page

--- adopt-me/src/pages/DetailsPage.jsx
+++ adopt-me/src/pages/DetailsPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import Carousel from '../components/Carousel';
 
 class DetailsPage extends React.Component {
   //   constructor(props) {
@@ -27,14 +28,15 @@ class DetailsPage extends React.Component {
       return <p></p>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
 
     return (
       <div className='details'>
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>ADOPT {name}</button>
+          <button>Adopt {name}</button>
           <p>{description}</p>
         </div>
       </div>
