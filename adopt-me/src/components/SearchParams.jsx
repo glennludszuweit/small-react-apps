5b8380624414f035dad24c22ff112ba6688be232
glenn <glenn.ludszuweit@gmail.com>
2020-06-06 16:43:27 +0200

    added frontendmasters PET api

--- adopt-me/src/components/SearchParams.jsx
+++ adopt-me/src/components/SearchParams.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seatle, WA');
+  const [animal, setAnimal] = useState('dog');
 
   const setLocationHandler = (e) => setLocation(e.target.value);
+  const setAnimalHandler = (e) => setAnimal(e.target.value);
 
   return (
     <div className='search-params'>
@@ -18,6 +21,22 @@ const SearchParams = () => {
             onChange={setLocationHandler}
             onBlur={setLocationHandler}
           />
+        </label>
+        <label htmlFor='animal'>
+          Animal
+          <select
+            id='animal'
+            value={animal}
+            onChange={setAnimalHandler}
+            onBlur={setAnimalHandler}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
