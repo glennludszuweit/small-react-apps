b31acbb234145826b630fd4b6301972809ae7aa4
glenn <glenn.ludszuweit@gmail.com>
2020-06-06 16:34:17 +0200

    adopt-me configured eslint for react-hooks

--- adopt-me/src/components/SearchParams.jsx
+++ adopt-me/src/components/SearchParams.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-  const location = 'Seatle, WA';
+  const [location, setLocation] = useState('Seatle, WA');
+
+  const setLocationHandler = (e) => setLocation(e.target.value);
 
   return (
     <div className='search-params'>
+      <h1>{location}</h1>
       <form>
         <label htmlFor='localtion'>
           Location
-          <input id='location' value={location} placeholder='Location' />
+          <input
+            id='location'
+            value={location}
+            placeholder='Location'
+            onChange={setLocationHandler}
+            onBlur={setLocationHandler}
+          />
         </label>
         <button>Submit</button>
       </form>
