8d0db6e5a5acf15323f1b06cf1683513d95157cb
glenn <glenn.ludszuweit@gmail.com>
2020-06-07 22:26:51 +0200

    adopt-me setup Themes using context API

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Router, Link } from '@reach/router';
 import { render } from 'react-dom';
 import HomePage from './pages/HomePage';
 import DetailsPage from './pages/DetailsPage';
+import ThemeContext from './context/ThemeContext';
 
 const App = () => {
+  const themeHook = useState('darkblue');
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to='/'>Adopt Me!</Link>
-        </header>
-        <Router>
-          <HomePage path='/' />
-          <DetailsPage path='/details/:id' />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to='/'>Adopt Me!</Link>
+          </header>
+          <Router>
+            <HomePage path='/' />
+            <DetailsPage path='/details/:id' />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
