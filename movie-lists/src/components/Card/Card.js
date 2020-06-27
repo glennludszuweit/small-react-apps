84b307897c880828addaca20302d6207d5c9c7fc
glenn <glenn.ludszuweit@gmail.com>
2020-06-27 17:27:45 +0200

    movie-lists added Card component

---
+++ movie-lists/src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <h2>Movie #1</h2>
+    </div>
+  );
+}
+
+export default Card;
--- movie-lists/src/containers/List.js
+++ movie-lists/src/containers/List.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Card from '../components/Card/Card';
 
 class List extends Component {
   render() {
     return (
       <div>
-        <h1>Movie Lists</h1>
+        <Card />
       </div>
     );
   }
