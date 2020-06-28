50192c9cbfcb988711756a95fc4dd3c16782aedd
glenn <glenn.ludszuweit@gmail.com>
2020-06-28 13:24:00 +0200

    weather-app setup create react app and api keys

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const api = {
+  key: 'a85e39de77109814f0519b8d6bcea6ca',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+function App() {
+  return <div className='App'></div>;
+}
+
+export default App;
